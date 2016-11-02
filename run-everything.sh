444266453cc6063a092033a8a5be0792c5d08ccb
Natanael Arndt <arndtn@gmail.com>
2016-11-02 15:00:13 +0100

    Improve process management

--- run-everything.sh
+++ run-everything.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+kill_group() {
+    PGID=$1
+    echo "kill group: $PGID"
+    kill -TERM -$PGID
+}
+
+PGID=$( ps opgid= $$ | tr -d ' ' )
+
+echo "group is: "$PGID
+
+# Ctrl-C trap. Catches INT signal
+trap "kill_group $PGID; exit 0" INT
+
 for i in "01" "02" "03" "03" "04" "05" "06" "07" "08" "09" "10" ;
 do
     export i
@@ -8,14 +21,17 @@ do
     export PARAMS
 
     ./run-instance.sh
+    pgrep -g $PGID | xargs --no-run-if-empty ps fp;
 
     PARAMS="-nv"
     export PARAMS
 
     ./run-instance.sh
+    pgrep -g $PGID | xargs --no-run-if-empty ps fp;
 
     PARAMS="-gc"
     export PARAMS
 
     ./run-instance.sh
+    pgrep -g $PGID | xargs --no-run-if-empty ps fp;
 done
--- run-instance.sh
+++ run-instance.sh
@@ -1,5 +1,47 @@
 #!/bin/sh
 
+run_monitor () {
+    RUNDIR=$1
+    LOGDIR=$2
+    storePID=$3
+
+    file=$LOGDIR/mem-$RUNDIR
+    pid=$storePID
+
+    while true; do date +%s | awk '{ printf $1 " "}' | tee -a $file ; du -s $RUNDIR | awk '{ printf $1 " "}' | tee -a $file; ps -o rss $pid | grep -v RSS | tee -a $file ; sleep 2 ;done
+}
+
+run_store () {
+    PARAM=$1
+    quit-store $PARAM &
+    storePID=$!
+    return $storePID
+}
+
+run_bsbm () {
+    RUNDIR=$1
+    LOGDIR=$2
+
+    cd ../bsbmtools-0.2/
+    ./testdriver http://localhost:5000/sparql -runs 200 -w 40 -dg "urn:bsbm" -o ../quit-eval/$LOGDIR/$RUNDIR.xml -ucf usecases/exploreAndUpdate/sparql.txt -udataset dataset_update.nt -u http://localhost:5000/sparql
+    cp run.log ../quit-eval/$LOGDIR/$RUNDIR-run.log
+    cd ../quit-eval/
+}
+
+terminate () {
+    storePID=$1
+    echo "terminate instance of store: $storePID"
+    pgrep -P $storePID | xargs --no-run-if-empty ps fp;
+    CPIDS=$(pgrep -P $storePID); kill -TERM $storePID $CPIDS
+    #CPIDS=$(pgrep -P $storePID); (sleep 5 && kill -KILL $storePID $CPIDS &); kill -TERM $storePID $CPIDS
+    echo "wait for store to terminate"
+    wait $storePID
+    echo "terminated store: $storePID"
+}
+
+#trap 'kill $(jobs -p)' EXIT SIGINT
+#trap "terminate $storePID $watchPID; exit 0" INT
+
 RUNDIR="quit"$PARAMS"-"$i
 LOGDIR=$RUNDIR"-logs"
 
@@ -15,30 +57,24 @@ git add .gitattributes config.ttl graph.nq
 git commit -m "init graph"
 git tag init-graph
 
-quit-store $PARAMS &
-storePID=$!
+run_store $PARAMS
+storePID=$?
 echo "store: " $storePID
 
 cd ..
 
 sleep 10
 
-export storePID
-export LOGDIR
-export RUNDIR
-./watch.sh &
+run_monitor $RUNDIR $LOGDIR $storePID &
 watchPID=$!
 echo "watch: " $watchPID
 
 sleep 10
 
-cd ../bsbmtools-0.2/
-./testdriver http://localhost:5000/sparql -runs 200 -w 40 -dg "urn:bsbm" -o ../quit-eval/$LOGDIR/$RUNDIR.xml -ucf usecases/exploreAndUpdate/sparql.txt -udataset dataset_update.nt -u http://localhost:5000/sparql
-cp run.log ../quit-eval/$LOGDIR/$RUNDIR-run.log
-cd ../quit-eval/
+run_bsbm $RUNDIR $LOGDIR
 
 sleep 10
 
-kill $watchPID
-sleep 2
-pkill -TERM -P $storePID
+terminate $storePID
+
+exit 0
