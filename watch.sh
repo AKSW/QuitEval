d5de6b969a32e5be1717722e5a95ccacc462137a
Natanael Arndt <arndtn@gmail.com>
2016-11-07 15:24:31 +0100

    Remove unused watch.sh

--- watch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-file=$LOGDIR/mem-$RUNDIR pid=$storePID ; while true; do date +%s | awk '{ printf $1 " "}' | tee -a $file ; du -s $RUNDIR | awk '{ printf $1 " "}' | tee -a $file; ps -o rss $pid | grep -v RSS | tee -a $file ; sleep 2 ;done
