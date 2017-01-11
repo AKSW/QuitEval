48827f66e18787711fcb0f9b8c899aaf5bd61b4f
Natanael Arndt <arndtn@gmail.com>
2017-01-11 17:27:38 +0100

    Add output for maximum number of statements and dryRun option for trim script

--- trim.py
+++ trim.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     """
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('--dryCount', action='store_true')
     parser.add_argument('input', type=str)
     parser.add_argument('output', type=str)
 
@@ -16,18 +17,24 @@ if __name__ == "__main__":
     infile = open(args.input, 'r')
     outfile = open(args.output, 'w')
 
+    dryCount = args.dryCount
+
     count = 0
+    maxCount = 0
     newlines = []
     for line in list(infile):
         newlines.append(line)
         if (line.strip() == "#__SEP__"):
             print ("separate after", count, "lines")
-            if (count > 289):
-                print ("cut at", 289, "lines")
-                outfile.writelines(newlines[:289])
-                outfile.writelines(newlines[-2:])
-            else:
-                outfile.writelines(newlines)
+            if count > maxCount:
+                maxCount = count
+            if (not dryCount):
+                if (count > 289):
+                    print ("cut at", 289, "lines")
+                    outfile.writelines(newlines[:289])
+                    outfile.writelines(newlines[-2:])
+                else:
+                    outfile.writelines(newlines)
             count = 0
             newlines = []
         elif line.strip() == "":
@@ -36,3 +43,4 @@ if __name__ == "__main__":
             count += 1;
     infile.close()
     outfile.close()
+    print ("max number of statements is", maxCount)
