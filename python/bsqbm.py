046a55c6cdfe2fdcc74f4b24c95c91101d9c8a7b
Natanael Arndt <arndtn@gmail.com>
2017-07-26 17:25:53 +0200

    make it run for now

--- python/bsqbm.py
+++ python/bsqbm.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import signal
+import shutil
 import yaml
 import subprocess
 import shlex
@@ -11,11 +12,20 @@ import datetime
 from memory_profiler import memory_usage, LogFile
 import psutil
 import threading
+import pygit2
 
 class BSQBMRunner:
     executionQueue = []
+    prepared = False
+
+    def prepare(self):
+        for execution in self.executionQueue:
+            execution.prepare()
+        self.prepared = True
 
     def run(self, block = False):
+        if not self.prepared:
+            raise Exception("The Run was not prepared")
         for execution in self.executionQueue:
             execution.run(block)
             if (block):
@@ -42,22 +52,71 @@ class Execution:
     repositoryPath = None
     logPath = None
     quitArgs = None
+    bareRepo = None
+    configGarbageCollection = None
 
-    def run(self, block = False):
+    def prepare(self):
 
-        print ("start scenario {} with configuration:".format(self.runName))
+        print ("prepare scenario \"{}\" with configuration:".format(self.runName))
         print ("quit:", self.quitExecutable)
         print ("bsbm:", self.bsbmLocation)
         print ("bsbm config: runs={} warmup={}".format(self.bsbmRuns, self.bsbmWarmup))
         print ("repositoryPath:", self.repositoryPath)
         print ("logPath:", self.logPath)
         print ("args:", self.quitArgs)
+        print ("bareRepo:", self.bareRepo)
+        print ("configGarbageCollection:", self.configGarbageCollection)
         print ()
 
-        return
+        os.makedirs(self.logPath, exist_ok=True)
+        os.makedirs(self.repositoryPath, exist_ok=True)
+
+        if self.bareRepo:
+            self.prepare_repository()
+        else:
+            self.prepare_repository(self.repositoryPath)
+
+    def prepare_repository(self, directory):
+        repo = pygit2.init_repository(directory) # git init $directory
+        if self.configGarbageCollection:
+            repo.config.set_multivar("gc.auto", "*", 257)
+        #gitattributes = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "stuff", ".gitattributes")
+        #shutil.copy(gitattributes, directory)
+        #cp stuff/.gitattributes $directory/
+        configttl= os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "stuff", "config.ttl")
+        shutil.copy(configttl, os.path.join(self.repositoryPath, "config.ttl"))
+
+        # sed "s/.$/<urn:bsbm> ./g" $BSBM_DIR/dataset.nt | LC_ALL=C sort -u > $REPOSITORY/graph.nq
+        with open(os.path.join(self.bsbmLocation, "dataset.nt"), 'r') as sourceGraph:
+            with open(os.path.join(directory, "graph.nq"), 'w') as targetGraph:
+                for line in sorted(list(sourceGraph)):
+                    targetGraph.write(line.rstrip()[:-1] + "<urn:bsbm> .\n")
+
+        with open(os.path.join(directory, "graph.nq.graph"), 'w') as targetGraphDotGraph:
+                 targetGraphDotGraph.write("urn:bsbm\n")
+
+        index = repo.index
+        index.read()
+        index.add("graph.nq")
+        index.add("graph.nq.graph")
+        index.add("config.ttl")
+        index.write()
+        tree = index.write_tree()
+        author = pygit2.Signature("bsqbm", "bsqbm@experiment.example.org")
+        commiter = author
+        oid = repo.create_commit("HEAD", author, commiter, "init for bsqbm", tree, [])
+        print(type(oid), oid, str(oid), str(oid)[:5])
+        #repo.create_tag("init-graph", str(oid)[:5], pygit2.GIT_OBJ_BLOB, author, "init-graph\n") # git tag init-graph
+
+
+    def run(self, block = False):
+
+        print ("start scenario {}".format(self.runName))
+        print ()
+
+        # return
 
         self.running = True
-        os.makedirs(self.logPath, exist_ok=True)
         self.runQuit()
         monitor = threading.Thread(target=self.runMonitor, args=(self.quitProcess, self.repositoryPath))
         monitor.start()
@@ -70,18 +129,19 @@ class Execution:
 
     def runQuit(self):
         quitArgs = shlex.split(self.quitArgs)
-        print("Start", self.quitExecutable, "with", quitArgs, ": (", [self.quitExecutable] + quitArgs, ")")
-        self.quitProcess = subprocess.Popen([self.quitExecutable, "-t", self.repositoryPath] + quitArgs)
+        quitCommand = [self.quitExecutable, "-cm", "localconfig", "-c", os.path.join(self.repositoryPath, "config.ttl"), "-t", self.repositoryPath] + quitArgs
+        print("Start quit:", quitCommand)
+        self.quitProcess = subprocess.Popen(quitCommand)
         # "mprof", "run", "--multiprocess",
         print(self.quitProcess.pid)
 
     def runMonitor(self, process, directory):
-        print("Start monitor on pid: {} in direcotry: {}".format(self.quitProcess.pid, self.repositoryPath))
+        print("Start monitor on pid: {} in directory: {}".format(self.quitProcess.pid, self.repositoryPath))
         # self.memory_log = open("memory_log.txt", "w", encoding="utf-8")
         # self.mem_usage = memory_usage(self.quitProcess.pid, interval=.1, include_children=True, stream=self.memory_log)
         # print(self.mem_usage)
         # sys.stdout = LogFile('memory_profile_log')
-        reslog = open(self.logPath + "resources-mem.log", "w")
+        reslog = open(os.path.join(self.logPath, "resources-mem.log"), "w")
         psProcess = psutil.Process(process.pid)
         while(process.poll() == None):
             # timestamp = datetime.datetime.now()
@@ -105,10 +165,12 @@ class Execution:
         return total_size/1024
 
     def runBSBM(self):
-        arguments = "http://localhost:5000/sparql -runs {} -w {} -dg \"urn:bsbm\" -o {} -ucf usecases/exploreAndUpdate/sparql.txt -udataset dataset_update.nt -u http://localhost:5000/sparql".format(
+        arguments = "{} -runs {} -w {} -dg \"urn:bsbm\" -o {} -ucf usecases/exploreAndUpdate/sparql.txt -udataset dataset_update.nt -u {}".format(
+            "http://localhost:5000/sparql",
             self.bsbmRuns,
             self.bsbmWarmup,
-            os.path.abspath(self.logPath + self.runName + ".xml")
+            os.path.abspath(os.path.join(self.logPath, self.runName + ".xml")),
+            "http://localhost:5000/sparql"
         )
         self.bsbmArgs = shlex.split(arguments)
         print("Start BSBM in", self.bsbmLocation, "with", arguments)
@@ -124,11 +186,13 @@ class Execution:
         if self.running:
             # print(self.mem_usage)
             #self.memory_log.close()
-            self.terminateProcess(self.bsbmProcess)
+            if hasattr(self, "bsbmProcess"):
+                self.terminateProcess(self.bsbmProcess)
             # mv bsbm/run.log $QUIT_EVAL_DIR/$LOGDIR/$RUNDIR-run.log
-            if (os.path.exists(self.bsbmLocation + "run.log")):
-                os.rename(self.bsbmLocation + "run.log", self.logPath + self.runName + "-run.log")
-            self.terminateProcess(self.quitProcess)
+            if (os.path.exists(os.path.join(self.bsbmLocation, "run.log"))):
+                os.rename(os.path.join(self.bsbmLocation, "run.log"), os.path.join(self.logPath, self.runName + "-run.log"))
+            if hasattr(self, "quitProcess"):
+                self.terminateProcess(self.quitProcess)
             self.running = False
 
     def terminateProcess(self, process):
@@ -167,7 +231,7 @@ def main(scenarioPath):
 
     signal.signal(signal.SIGINT, signal_handler)
 
-    print(scenarioPath)
+    print("Use scenario configuration from:", scenarioPath)
 
     stream = open(scenarioPath, "r")
     docs = yaml.safe_load(stream)
@@ -183,7 +247,8 @@ def main(scenarioPath):
     repositoryBasePath = docs["repositoryBasePath"] if "repositoryBasePath" in docs else "repo"
     logBasePath = docs["logBasePath"] if "logBasePath" in docs else "logs"
 
-    bareRepo = docs["bareRepo"]
+    bareRepo = docs["bareRepo"] if "bareRepo" in docs else False
+    configGarbageCollection = docs["configGarbageCollection"] if "configGarbageCollection" in docs else False
 
     runner = BSQBMRunner()
 
@@ -199,19 +264,21 @@ def main(scenarioPath):
             execution.bsbmWarmup = bsbmWarmup
 
             # these parameters are individual per scenario
-            execution.runName = runName
             runDirectory = os.path.join(resultDirectory, runName)
             getScenarioPath = getScenarioPathFunction(runName, runDirectory, runConfig)
 
+            execution.runName = runName
             execution.repositoryPath = getScenarioPath("repositoryBasePath", repositoryBasePath)
             execution.logPath = getScenarioPath("logBasePath", logBasePath)
+            execution.quitArgs = runConfig["storeArguments"] if "storeArguments" in runConfig else ""
+            execution.bareRepo = runConfig["bareRepo"] if "bareRepo" in runConfig else bareRepo
+            execution.configGarbageCollection = runConfig["configGarbageCollection"] if "configGarbageCollection" in runConfig else configGarbageCollection
 
-            storeArguments = runConfig["storeArguments"] if "storeArguments" in runConfig else ""
-            execution.quitArgs = storeArguments
 
             runner.addExecutionToQueue(execution)
 
     # start benchmarks
+    runner.prepare()
     runner.run(block = True)
 
 if __name__ == '__main__':
